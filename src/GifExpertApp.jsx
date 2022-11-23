621a603c5997da518eccf3be6116cf0d9d87e43a
Cesar <cesar23carranza@gmail.com>
2022-11-23 12:28:03 -0600

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react';
+import { AddCategory, GifGrid } from './components';
+
+
+export const GifExpertApp = () => {
+
+    const [ categories, setcategories ] = useState([ 'One Punch' ]);
+
+    const onAddCategory = ( newCategory ) => {
+
+        if ( categories.includes(newCategory) ) return;
+              
+        //categories.push(newCategory);
+        
+         setcategories( [ newCategory, ...categories ] );
+        // setcategories( cat => [ ...cat, 'Valorant' ] );
+    }
+
+
+  return (
+    <>
+        
+        <h1>GifExpertApp</h1>
+
+     
+        <AddCategory 
+            onNewCategory={ (value) => onAddCategory(value) }
+        
+        />
+                
+        { 
+        categories.map( ( category ) => (
+                    <GifGrid 
+                        key={ category } 
+                        category={ category } />
+                ))
+        }
+            
+                 
+    </>
+  )
+}
+
+
---
+++ src/helpers/getGifs.js
@@ -0,0 +1,14 @@
+export const getGifs = async( category ) => {
+
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=J6VL0Jn6VkMm886tl61ojcZdPuUUCd97&q=${ category }&limit=10`
+    const resp = await fetch( url );
+    const { data } = await resp.json();
+
+    const gifs = data.map( img => ({
+        id: img.id,
+        title: img.title,
+        url: img.images.downsized_medium.url
+    }));
+
+    return gifs;
+}
